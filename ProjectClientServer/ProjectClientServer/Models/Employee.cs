891005b103d5c239f8fa24f280582842a220d548
Lathifah Hanum <lathifahhanum4@gmail.com>
2023-04-18 10:17:01 +0700

    update controller

--- ProjectClientServer/ProjectClientServer/Models/Employee.cs
+++ ProjectClientServer/ProjectClientServer/Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ProjectClientServer.Models;
 
@@ -21,8 +22,10 @@ public partial class Employee
 
     public string PhoneNumber { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual Account? TbMAccount { get; set; }
 
+    [JsonIgnore]
     public virtual Profiling? TbTrProfiling { get; set; }
 }
 
